c8052d03da7b1539df6b5c07cdb2c9b44766b201
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-05-21 17:22:27 -0500

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -5,11 +5,12 @@ import Comment from "./Comment"
 export default class BlogPost extends Component {
   render() {
     
-    const comments = [
-      "When we speak we are afraid our words will not be heard or welcomed. But when we are silent, we are still afraid. So it is better to speak. - Audrey Lorde", 
-      "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
-      "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
-    ]
+    const comments = 
+      [
+        "When we speak we are afraid our words will not be heard or welcomed. But when we are silent, we are still afraid. So it is better to speak. - Audrey Lorde",
+        "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
+        "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
+      ]
     
     return (
       <div id="blog-post" className="wrapper">
@@ -21,8 +22,8 @@ export default class BlogPost extends Component {
 
         
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
        
       </div>
     );
